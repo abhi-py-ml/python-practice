181a6bfacbf2d1235747befeffb64386b42d896b
abhijeet <katiyarabhi112@gmail.com>
2026-02-23 18:50:01 +0530

    Update calculator.py
    Added input validation and operation selection

--- calculator.py
+++ calculator.py
@@ -1,7 +1,24 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+num1 = (input())
+num2 = (input())
 
-print("Addition:", a + b)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
-print("Division:", a / b)
+try:
+    num1=float(num1)
+    num2=float(num2)
+except:
+    print("Enter a Valid Number")
+    exit()
+
+Choose=input("Enter Arthematic Operation. (add/sub/multi/divd): ").lower()
+if Choose not in ["add","sub","multi","divd"]:
+    print("Error : Enter valid input")
+
+else:
+    print("Calculating...")
+    if Choose == "add":
+        print(num1+num2)
+    elif Choose == "sub":
+        print(num1-num2)
+    elif Choose == "multi":
+        print(num1*num2)
+    else:
+        print(num1/num2)
