f0c8aa29df9753633e16f0db53b3dc28f20a3834
abhijeet <katiyarabhi112@gmail.com>
2026-02-24 15:50:27 +0530

    Update calculator.py
    added a while loop and corrected logic where a nomber divided by zero was showing traceback

--- calculator.py
+++ calculator.py
@@ -1,19 +1,22 @@
-num1 = (input())
-num2 = (input())
+while True:
+    num1 = (input())
+    num2 = (input())
 
-try:
-    num1=float(num1)
-    num2=float(num2)
-except:
-    print("Enter a Valid Number")
-    exit()
+    try:
+        num1=float(num1)
+        num2=float(num2)
+    except:
+        print("Enter a Valid Number")
+        continue
 
-Choose=input("Enter Arthematic Operation. (add/sub/multi/divd): ").lower()
-if Choose not in ["add","sub","multi","divd"]:
-    print("Error : Enter valid input")
+    Choose=input("Enter Arthematic Operation. (add/sub/multi/divd): ").lower()
+    if Choose not in ["add","sub","multi","divd"]:
+        print("Error : Enter valid input")
+        continue
 
-else:
-    print("Calculating...")
+    else:
+        print("Calculating...")
+    
     if Choose == "add":
         print(num1+num2)
     elif Choose == "sub":
@@ -21,4 +24,19 @@ else:
     elif Choose == "multi":
         print(num1*num2)
     else:
-        print(num1/num2)
+        if num2 == 0:
+            print("cannot be divided by 0")
+        else:
+            print(num1/num2)
+    ask=input("Continue? (yes/no)").lower()
+
+    if ask == "yes":
+        continue
+    elif ask == "no":
+        print("Program Ended.")
+        break
+    else:
+        print("Invalid input. Exiting.")
+        break
+
+            
