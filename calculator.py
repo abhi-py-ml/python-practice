17dcc533b5fddfbc212b46a9f5eb859f2028880b
abhijeet <katiyarabhi112@gmail.com>
2026-03-19 20:30:39 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,74 +1,87 @@
-history=[]
-def get_numbers():
-    while True:
-        try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-            return num1, num2
-        except ValueError:
-            print("Invalid number! Please try again.\n")
+class Calculator:
+    def __init__(self):
+        self.history = []
+
+    def user_input(self):
+        while True:
+            expression = input("Enter Expression: ").strip()
+
+            if expression in ['history', 'exit']:
+                return expression
+
+            if any(ch.isalpha() for ch in expression):
+                print("Enter valid expression!")
+                continue
+
+            return expression
+
+    def show_menu(self):
+        print("=" * 30)
+        print("      CLI CALCULATOR")
+        print("=" * 30)
+        print("Use operators: +  -  *  /  **  %")
+        print("history → Show history")
+        print("exit    → Quit program")
+
+    def calculate(self):
+        while True:
+            self.show_menu()
+
+            operator = self.user_input()
+
+            if operator == 'exit':
+                print("Program ended")
+                break
+
+            elif operator == 'history':
+                if not self.history:
+                    print("History is empty")
+                else:
+                    for item in self.history:
+                        print(item)
+                continue
+
+            try:
+                result = eval(operator)
+            except ZeroDivisionError:
+                print("Cannot divide by zero")
+                continue
+            except Exception:
+                print("Invalid expression!")
+                continue
+
+            print("Result:", result)
+            print("-" * 30)
+
+            self.history.append(f"{operator} = {result}")
 
 
-def calculate(num1, operation, num2):
-    if operation == "add":
-        return num1 + num2
-    elif operation == "sub":
-        return num1 - num2
-    elif operation == "multi":
-        return num1 * num2
-    elif operation == "divd":
-        if num2 == 0:
-            return "Cannot divide by 0"
-        return num1 / num2
-    elif operation == "power":
-        return num1 ** num2
-    elif operation == "mod":
-        return num1 % num2
 
-def show_menu():
-    print("\nChoose operation:")
-    print("add   → Addition")
-    print("sub   → Subtraction")
-    print("multi → Multiplication")
-    print("divd  → Division")
-    print("power → Power")
-    print("mod   → Modulus")
-    print("history → Show history")
-    print("exit → Quit program")
+cal = Calculator()
+cal.calculate()
 
-def main():
-    while True:
-        show_menu()
+
+            
+                
+
+        
         
 
-        operation = input("Enter operation : ").lower()
-        if operation == "exit":
-            print("program ended:")
-            break
-        if operation == "history":
-            if not history:
-                print("No calculations yet.")
-            else:
-                print("\nCalculation History:")
-                for item in history:
-                    print(item)
-            continue
-
-        if operation not in ["add","sub","multi","divd","power","mod"]:
-            print("Invalid operation!\n")
-            continue
-
-        num1, num2 = get_numbers()
-
-        result = calculate(num1, operation, num2)
-        print("Result:", result)
-
-        history.append(f"{num1} {operation} {num2} = {result}")
-
         
-            
 
 
 
-main()
-            
+
+
+
+
+
+
+
+
+
+    
+        
+    
+
+      
